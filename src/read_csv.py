6aed9d3824f9cd7b13b770c045321308b95d5062
ben tan <btan@esri.com>
2017-06-18 21:29:07 +0800

    add files

---
+++ src/read_csv.py
@@ -0,0 +1,23 @@
+import csv
+import plotly
+
+plotly.tools.set_credentials_file(username='iuhsihsow',
+                                  api_key='5zShtnMXkkhxiqp4MB4w')
+
+plotly.tools.set_config_file(world_readable=False,
+                             sharing='private')
+
+file_name = "../data/beijing_house_price.csv"
+record_count = 0
+
+max_price = 0
+min_price = 10000000000
+with open(file_name, encoding='utf-8') as csvfile:
+    reader = csv.DictReader(csvfile)
+    for row in reader:
+        record_count += 1
+        print(row['id'], row['X'], row['Y'], row['price'])
+        max_price = max((int)(row['price']), max_price)
+        min_price = min((int)(row['price']), min_price)
+
+    print(record_count, max_price, min_price)
