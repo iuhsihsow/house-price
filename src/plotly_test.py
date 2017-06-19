36f113afacc4279c9b811539f45fac5ca4e301dc
ben tan <btan@esri.com>
2017-06-19 10:34:28 +0800

    add new test file

---
+++ src/plotly_test.py
@@ -0,0 +1,7 @@
+import plotly
+
+plotly.tools.set_credentials_file(username='iuhsihsow',
+                                  api_key='5zShtnMXkkhxiqp4MB4w')
+
+plotly.tools.set_config_file(world_readable=False,
+                             sharing='private')
--- src/read_csv.py
+++ src/read_csv.py
@@ -1,11 +1,4 @@
 import csv
-import plotly
-
-plotly.tools.set_credentials_file(username='iuhsihsow',
-                                  api_key='5zShtnMXkkhxiqp4MB4w')
-
-plotly.tools.set_config_file(world_readable=False,
-                             sharing='private')
 
 file_name = "../data/beijing_house_price.csv"
 record_count = 0
