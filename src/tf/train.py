8cdbb3ae9bb40f58e3f0606d25c413e128d1652f
iuhsihsow <274092950@qq.com>
2017-12-07 17:20:05 +0800

    add graphic

--- src/tf/train.py
+++ src/tf/train.py
@@ -2,6 +2,7 @@ import tensorflow as tf
 import numpy as np
 
 import csv
+import matplotlib.pyplot as plt
 
 input_file = "../../data/house_price_number.csv"
 
@@ -16,6 +17,7 @@ pmax = 154953.0
 count = 5876
 
 pos_np = np.zeros((count, 2), dtype=np.float32)
+index_np = np.zeros((count,1), dtype=np.float32)
 price_np = np.zeros((count, 1), dtype=np.float32)
 
 index = 0
@@ -24,6 +26,7 @@ with open(input_file, 'rt') as ifile:
 	for row in reader:
 		pos_np[index] = [float(row['X']) - x_center, float(row['Y']) - y_center]
 		price_np[index] = float(row['price'])/pmax
+		index_np[index]=index
 		index += 1
 print 'pos_np shape is:' , pos_np.shape
 
@@ -42,20 +45,48 @@ def add_layer(inputs, in_size, out_size, activation_function=None):
 	return outputs
 
 
-cluster = 10
+cluster = 50
 l1 = add_layer(xs, 2, cluster, activation_function=tf.nn.relu)
 prediction = add_layer(l1, cluster, 1, activation_function=None)
 loss = tf.reduce_mean(tf.reduce_sum(tf.square(ys - prediction), reduction_indices=[1]))
 
-optimizer = tf.train.GradientDescentOptimizer(0.1)
+
+optimizer = tf.train.GradientDescentOptimizer(0.05)
 train_step = optimizer.minimize(loss)
 
 init = tf.initialize_all_variables()
+saver = tf.train.Saver()
 sess = tf.Session()
 sess.run(init)
 
 for i in range(1000):
 	sess.run(train_step, feed_dict={xs: pos_np, ys: price_np})
-	if i % 50 == 0:
+	if i % 10 == 0:
 		print sess.run(loss, feed_dict={xs: pos_np, ys: price_np})
+
+save_path = saver.save(sess, "../../data/tf.ckpt")
+print "save to ", save_path
+
+feed_dict = {xs: pos_np}
+res = sess.run(prediction, feed_dict)
+
+rate = 0.1
+
+i = 0
+for x in range(0, count):
+	if (abs(res[x] - price_np[x]) / price_np[x]) < rate:
+		i += 1
+print 'good result:', i 
+
+show_np = np.zeros((i, 2), dtype=np.float32)
+i = 0
+for x in range(0, count):
+	if (abs(res[x] - price_np[x]) / price_np[x]) < rate:
+		show_np[i] = [price_np[x], (abs(res[x] - price_np[x]) / price_np[x])]
+		i += 1
+
+print 'Mean:', np.mean(show_np, axis=0)
+plt.scatter(show_np[:, 0] * pmax, show_np[:, 1], label='Fitting Line')
+#plt.scatter(price_np * pmax, abs(price_np - res)/price_np, label='Fitting Line')
+plt.show()
 
